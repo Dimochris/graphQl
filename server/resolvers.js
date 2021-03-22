7e5753eda5429cd17b0cf9396933cf4992d1fec5
Dimochris <chrisdimo87@gmail.com>
2021-03-22 15:13:07 +0200

    message

---
+++ server/resolvers.js
@@ -0,0 +1,23 @@
+const db = require("./db");
+const Query = {
+  company: (root, { id }) => db.companies.get(id),
+  job: (root, { id }) => db.jobs.get(id),
+  jobs: () => db.jobs.list(),
+};
+const Mutation = {
+  createJob: (root, { companyId, title, description }) => {
+    const id = db.jobs.create({ companyId, title, description });
+    return db.jobs.get(id);
+  },
+};
+
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.companyId),
+};
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Mutation, Job, Company };
